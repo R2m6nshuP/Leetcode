ae636b1926868b37749f0e9517039df29b9e8ba9
R2m6nshuP <rra443564@gmail.com>
2026-06-23 15:01:26 +0530

    Time: 0 ms (100%), Space: 9.8 MB (49.31%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+    if(s.size()!=t.size()) return 0;
+    int freq[26]={0};
+    for(int i=0;i<s.size();i++){
+        freq[s[i]-'a']++;
+
+    }
+    for(int i=0;i<s.size();i++){
+        freq[t[i]-'a']--;
+        
+    }
+    for(int i=0;i<26;i++){
+        if(freq[i]!=0) return 0;
+        
+    }
+    return 1;
+    }
+};
