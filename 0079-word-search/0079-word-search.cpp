dcac1e7d8c064a48f5fd343369e6f37011b949c8
R2m6nshuP <rra443564@gmail.com>
2026-07-12 18:12:34 +0530

    Time: 243 ms (56.59%), Space: 10.5 MB (82.09%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void finder(vector<vector<char>>& board,vector<vector<char>>& copy,string &word,int j,int x,int y,bool &ans){
+    void finder(vector<vector<char>>& board,string &word,int j,int x,int y,bool &ans){
         if(ans) return;
         if(j==word.size()){
             ans=1;
@@ -9,25 +9,26 @@ public:
         if(y<0 || x<0) return;
         if(y==board[0].size() || x==board.size()) return;
         if(board[x][y]!=word[j]) return;
+        char temp=board[x][y];
         board[x][y]='0';
-        finder(board,copy,word,j+1,x+1,y,ans);
-        finder(board,copy,word,j+1,x,y+1,ans);
-        finder(board,copy,word,j+1,x-1,y,ans);
-        finder(board,copy,word,j+1,x,y-1,ans);
-        if(!ans) board[x][y]=copy[x][y];
+        finder(board,word,j+1,x+1,y,ans);
+        finder(board,word,j+1,x,y+1,ans);
+        finder(board,word,j+1,x-1,y,ans);
+        finder(board,word,j+1,x,y-1,ans);
+        board[x][y]=temp;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        vector<vector<char>> copy=board;
         bool ans=0;
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
                 if(word[0]==board[i][j]) {
+                    char temp=board[i][j];
                     board[i][j]='0';
-                    finder(board,copy,word,1,i+1,j,ans);
-                    finder(board,copy,word,1,i,j+1,ans);
-                    finder(board,copy,word,1,i-1,j,ans);
-                    finder(board,copy,word,1,i,j-1,ans);
-                    if(!ans) board[i][j]=copy[i][j];
+                    finder(board,word,1,i+1,j,ans);
+                    finder(board,word,1,i,j+1,ans);
+                    finder(board,word,1,i-1,j,ans);
+                    finder(board,word,1,i,j-1,ans);
+                    board[i][j]=temp;
                 }
             }
         }
