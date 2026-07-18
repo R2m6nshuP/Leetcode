bb7810ba318979aeeed74e1147667015238857fb
R2m6nshuP <rra443564@gmail.com>
2026-07-18 16:52:15 +0530

    Time: 0 ms (100%), Space: 8.3 MB (16.3%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string toLowerCase(string &s) {
+        for(int i=0;i<s.size();i++){
+            if(s[i]>=65 && s[i]<=90) s[i]+=('a'-'A');
+        }
+        return s;
+    }
+};
