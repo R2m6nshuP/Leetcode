2797e893e298f8c350000d96f95f0b21c26d5c80
R2m6nshuP <rra443564@gmail.com>
2026-08-10 21:04:25 +0530

    Time: 5 ms (28.76%), Space: 15.3 MB (13.04%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -3,6 +3,7 @@ public:
     string simplifyPath(string path) {
         int i=0;
         int j=1;
+        path+="/";
         stack<string> st;
         while(j<path.size()){
             if(path[j]=='/') {
@@ -21,15 +22,7 @@ public:
                 j++;
             }
         }
-        if(i<path.size()-1 && j==path.size()){
-                string s=path.substr(i,j-i);
-                if(s=="/") ; 
-                else if(s=="/.") ;
-                else if(s=="/..") {
-                    if(!st.empty()) st.pop();
-                }
-                else st.push(s);
-        }
+        
         string ans="";
         while(!st.empty()){
             ans=st.top()+ans;
